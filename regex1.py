e3ca4da235ff26d776e7f9cc5ef6d249355efc77
Abhay Trivedi <abhaytrivedi3@gmail.com>
2013-09-14 12:35:47 +0530

    I am adding this from to test out master merge

---
+++ regex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import re
+
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+      print(re.sub('(Len|Neverm)ore', '###', line), end='')
+
+if __name__ == "__main__": main()
